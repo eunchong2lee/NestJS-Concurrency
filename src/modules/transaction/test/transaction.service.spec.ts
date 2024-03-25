3de8d544adcce1b68167cb279b43682fd74d388a
eunchong2lee <tlfqjrjs1234@naver.com>
2024-03-25 13:58:50 +0900

    Update: transaction service spec  by setting

--- src/modules/transaction/test/transaction.service.spec.ts
+++ src/modules/transaction/test/transaction.service.spec.ts
@@ -1,15 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionService } from '../transaction.service';
+import { Repository } from 'typeorm';
+import { Mock } from '../../mock/entities/mock.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockRepository } from '../../mock/mock.repository';
 
 describe('TransactionService', () => {
   let service: TransactionService;
+  let mockRepository: MockRepository;
+  const dataSource = {
+    createEntityManager: jest.fn(),
+    createQueryRunner: jest.fn().mockReturnValue({
+      connect: jest.fn(),
+      manager: {
+        update: jest.fn(),
+      },
+      startTransaction: jest.fn(),
+      commitTransaction: jest.fn(),
+      rollbackTransaction: jest.fn(),
+      release: jest.fn(),
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionService],
+      providers: [
+        TransactionService,
+        {
+          provide: MockRepository,
+          useValue: {
+            dataSource,
+            save: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionService>(TransactionService);
+    mockRepository = module.get<MockRepository>(MockRepository);
   });
 
   it('should be defined', () => {
